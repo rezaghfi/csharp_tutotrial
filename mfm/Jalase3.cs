c4e3a5652697e060c168150e985f0474736974fb
reza <reza.gh.fi@gmail.com>
2023-03-02 17:16:53 +0330

    Jalase4

--- mfm/Jalase3.cs
+++ mfm/Jalase3.cs
@@ -1,40 +1,43 @@
 using System.Net.Sockets;
 using System.Reflection.PortableExecutable;
 
-namespace csharp_tutorial;
-
-public class Jalase3
+namespace mfm
 {
-    // method
-    public static void jalase3()
+    public class Jalase3
     {
-        // call method
-        //function();
-        method1(2,4);
-        int z = method2(2,4);
-        int[] car;
-    }
+        // method
+        public static void jalase3()
+        {
+            // call method
+            //function();
+            method1(2,4);
+            int z = method2(2,4);
+            int[] car;
+        }
 
-    public static void function()
-    {
-        int a = 10;
-        int b = 5;
-        int z = a + b;
-        Console.WriteLine(z);
-    }
+        public static void function()
+        {
+            int a = 10;
+            int b = 5;
+            int z = a + b;
+            Console.WriteLine(z);
+        }
 
-    public static void method1(int a, int b)
-    {
-        int z = a + b;
-        Console.WriteLine(z);
-    }
+        public static void method1(int a, int b)
+        {
+            int z = a + b;
+            Console.WriteLine(z);
+        }
 
-    public static int method2(int a, int b)
-    {
-        return a + b;
-    }
+        public static int method2(int a, int b)
+        {
+            return a + b;
+        }
     
     
     
+    }
+
+
 }
 
---
+++ mfm/Jalase4.cs
@@ -0,0 +1,40 @@
+﻿
+namespace mfm
+{
+    public class Jalase4
+    {
+        void f1(int a)
+        {
+            //
+        }
+
+        void f2(int a)
+        {
+            
+        }
+        
+        // method
+        public static int function(int input)
+        {
+            // body
+            return input + 2;
+        }
+
+        public static int[] date(int input)
+        {
+            int maghA1, bagh1, maghA2, bagh2, year = 2023, month = 1, day = 1;
+            maghA1 = input / 365;
+            bagh1 = input % 365;
+            year += maghA1;
+            maghA2 = bagh1 / 30;
+            bagh2 = bagh1 % 30;
+            month += maghA2;
+            day += bagh2;
+            int[] a = { year, month, day };
+            return a;
+        }
+    }
+    
+    
+}
+
