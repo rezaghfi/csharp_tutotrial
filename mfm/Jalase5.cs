58fc5c05a23ec5fc50bb62b23a81f8146318251b
reza <reza.gh.fi@gmail.com>
2023-03-05 17:04:53 +0330

    jalase 5

---
+++ mfm/Jalase5.cs
@@ -0,0 +1,158 @@
+﻿namespace MFM;
+
+public class Jalase5
+{
+    // method
+    public void jalase5(int a)
+    {
+        if (a > 0)
+        {
+            Console.WriteLine(a);
+        }
+        else
+        {
+            Console.WriteLine(a);
+            
+        }
+    }
+    
+    // overloading
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+    public double add(double a, double b)
+    {
+        // double ->int
+        int c = (int)a;
+        // int -> double
+        a = b;
+        return (int)(a + b);
+    }
+
+    public int add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+}
+
+public struct Animal
+{
+    // field
+    public string kind;
+    string color;
+    
+    // method
+    void run()
+    {
+        Console.WriteLine("run");
+        Vehicle v = new Vehicle("red","benz", "i5");
+        v.color = "blue";
+        
+        
+    }
+    
+    
+}
+
+public class Vehicle
+{
+    // access
+    public string color;
+    public string model;
+    public string company;
+    
+    // constractor
+    public Vehicle(string color, string company, string model)
+    {
+        this.color = color;
+        this.company = company;
+        this.model = model;
+        Console.WriteLine("it is new Vehicle");
+    }
+
+    public Vehicle()
+    {
+        Console.WriteLine("it is constructor without parameter");
+    }
+    public void run()
+    {
+        Console.WriteLine(this.model + " is running");
+    }
+    
+    // encpsulation
+    private string ecuName = "intel";
+    // property
+    public string EcuName
+    {
+        set;
+        get;
+    }
+
+    public void setEcuName(string input)
+    {
+        this.ecuName = input;
+        this.tier = "t";
+    }
+
+    public string getecuName()
+    {
+        return this.ecuName;
+    }
+    // برای دستری فرزندان
+    protected string tier;
+    
+    // intermal
+    internal string glassType;
+    
+    
+
+
+}
+
+
+public class Bicycle: Vehicle
+{
+    // Construcy
+    
+}
+
+public class Motor : Vehicle
+{
+    // constant field
+    public const int MAX = 10;
+    
+    
+    public static string staticFiled;
+    public Motor(string color, string company, string model)
+    {
+        this.color = color;
+        this.model = model;
+        this.company = company;
+    }
+
+    public Motor()
+    {
+        
+    }
+
+    // non static
+    public void stop()
+    {
+        this.tier = "kavirTier";
+        base.tier = "";
+        this.color = "red";
+        // constant
+        const int i = 0;
+
+    }
+
+    //static
+    public static void staticMethod()
+    {
+        Console.WriteLine("ana static method");
+        staticFiled = "static1";
+        //this.color
+    }
+    
+}
--- mfm/Run.cs
+++ mfm/Run.cs
@@ -1,21 +1,60 @@
-﻿using MFM;
+﻿using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using MFM;
+
+
 
 
 public class Run
 {
+    // block
+    public int i;
     public static void Main(string[] args)
     {
+        
         
         //Jalase2.one();
         //Jalase2.two();
         // Jalase3.jalase3();
-        int x = Jalase4.method(2);
-        Console.WriteLine(x);
-        int[] a = Jalase4.date(13);
-        Console.WriteLine("year:"+a[0]+"-"+"month:"+a[1]+"-"+"day:"+a[2]);
-        Car car1 = new Car();
-        Car.run();
-        Car.s = 1;
+        // int x = Jalase4.method(2);
+        // Console.WriteLine(x);
+        // int[] a = Jalase4.date(13);
+        // Console.WriteLine("year:"+a[0]+"-"+"month:"+a[1]+"-"+"day:"+a[2]);
+        // Car car1 = new Car();
+        // Car.run();
+        // Car.s = 1;
+        // methof
+        // Jalase5 j5 = new Jalase5();
+        // j5.jalase5(-5);
+        // // over loading
+        // j5.add(1, 2);
+        // j5.add(1.2, 3.45);
+        // Animal bee = new Animal();
+        // bee.kind = "bee";
+        // class Object
+        // 
+        //string input = Console.ReadLine();
+        Vehicle v1 = new Vehicle();
+        Vehicle v2 = new Vehicle("red", "benz", "i5");
+        // v.color = "red";
+        // v.company = "benz";
+        // v.model = "i5";
+        v2.run();
+        v2.EcuName = "intel i7";
+        Console.WriteLine(v2.EcuName);
+        // getter setter
+        v2.setEcuName("intel i3");
+        v2.getecuName();
+
+        // in
+        Motor m = new Motor();
+        m.model = "kavir";
+        Motor m1 = new Motor("red","kavir","e3");
+        m.run();
+        Motor.staticMethod();
+        Motor.staticFiled = "static";
+        
+
     }
 }
 
